78be783df63dc12e97b11bcac66333fcc0f146f4
sneurlax <sneurlax@gmail.com>
2026-02-24 11:51:16 -0600

    chore: update generated plugin registrants and lockfile

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import webview_cef
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  WebviewCefPlugin.register(with: registry.registrar(forPlugin: "WebviewCefPlugin"))
 }
--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <webview_cef/webview_cef_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  WebviewCefPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WebviewCefPluginCApi"));
 }
